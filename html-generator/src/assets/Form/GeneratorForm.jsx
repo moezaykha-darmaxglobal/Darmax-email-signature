2c7c42ece01ab91dc98eb47abcafe6cb49d8890e
moezaykha-darmaxgloba <moe@darmaxglobal.com>
2023-03-21 16:50:59 +0630

    build 4

--- html-generator/src/assets/Form/GeneratorForm.jsx
+++ html-generator/src/assets/Form/GeneratorForm.jsx
@@ -7,35 +7,14 @@ import guideimg from '../guide1.png'
 import logodarmax from '../logodarmax.png'
 function GeneratorForm() {
   const [name, setName] = useState('');
-  const [greeting, setGreeting] = useState('');
+  
   const [image, setImage] = useState(null);
   const [imageUrl, setImageUrl] = useState('');
   const [htmlCode, setHtmlCode] = useState('');
   const [copied, setCopied] = useState(false);
-  const [position, setPosition] = useState('');
-  const [contact, setContact] = useState('');
-  const [email, setEmail] = useState('');
+
 
   const htmlRef = useRef(null);
-
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePositionChange = (e) => {
-    setPosition(e.target.value);
-  };
-
-  const handleContactChange = (e) => {
-    setContact(e.target.value);
-  };
-
-  const handleGreetingChange = (e) => {
-    setGreeting(e.target.value);
-  };
 
   const handleImageChange = (e) => {
     setImage(e.target.files[0]);
@@ -57,23 +36,19 @@ function GeneratorForm() {
       },
       () => {
         // Get the download URL of the uploaded image
+        // const url = await getDownloadURL(uploadTask.snapshot.ref);
         getDownloadURL(uploadTask.snapshot.ref).then((url) => {
           // Generate the HTML tag with the input values and image URL
-          const html = `<p>${greeting}</p>
-          <img width="200" height="60" src="${imageUrl}" alt="${name}" />
-          <p>${name}</p>
-          <p>${position}</p>
-          <img width="200" height="60" src="https://firebasestorage.googleapis.com/v0/b/email-template-generator-1ebab.appspot.com/o/image%2Fimage.png100bf532-96af-40a6-83c6-8dce7adc8344?alt=media&token=95230f47-fb58-46dc-8cdd-843c2bab7e80" alt="companylogo"/>
-          <p>Phone:${contact}</p>
-          <p>Email:${email}</p>
-          <br>
-          <p>Hong Kong, Wan Chai, Hennessy Rd, 8-12號, China Hong Kong Tower, 24樓</p>
-          <a href="https://www.darmaxglobal.com" target="_blank">www.darmaxglobal.com</a>`;
+          setImageUrl(url);
+          const html = `
+          <img width="200" height="60" src="${imageUrl}"  />
+          `;
           console.log(html);
           setHtmlCode(html);
           // Set the image URL state
-          setImageUrl(url);
-        });
+          
+        }
+        );
       }
     );
   };
@@ -97,24 +72,10 @@ function GeneratorForm() {
     <form onSubmit={handleFormSubmit}>
      
 
-      <label htmlFor="greeting">Greeting:</label>
-      <input type="text" id="greeting" name="greeting" value={greeting} onChange={handleGreetingChange} />
 
       <label htmlFor="image">Signature Image: <small>preferably with white background</small></label>
       <input type="file" id="image" name="image" accept="image/*" onChange={handleImageChange} />
       
-
-      <label htmlFor="name">Name:</label>
-      <input type="text" id="name" name="name" value={name} onChange={handleNameChange} />
-
-      <label htmlFor="position">Position:</label>
-      <input type="text" id="position" name="position" value={position} onChange={handlePositionChange} />
-
-      <label htmlFor="contact">Contact:</label>
-      <input type="text" id="contact" name="contact" value={contact} onChange={handleContactChange} />
-
-      <label htmlFor="email">Email:</label>
-      <input type="text" id="email" name="email" value={email} onChange={handleEmailChange} />
 
       <button type="submit">Generate HTML</button>
 
@@ -123,18 +84,8 @@ function GeneratorForm() {
           <p>Display:</p>
           <div dangerouslySetInnerHTML={{ __html: htmlCode }}></div>
           <pre id="generated-html" >
-            {`<p>${greeting}</p>
-            <img width="200" height="60" src="${imageUrl}" alt="${name}" />
-            <p>${name}</p>
-            <p>${position}</p>
-            <img width="200" height="60" src="https://firebasestorage.googleapis.com/v0/b/email-template-generator-1ebab.appspot.com/o/image%2Fimage.png100bf532-96af-40a6-83c6-8dce7adc8344?alt=media&token=95230f47-fb58-46dc-8cdd-843c2bab7e80" alt="companylogo"/>
-            <p>Phone:${contact}</p>
-            <p>Email:${email}</p>
-            <br>
-            <p>Hong Kong, Wan Chai, Hennessy Rd, 8-12號, China Hong Kong Tower, 24樓</p>
-            <a href="https://www.darmaxglobal.com" target="_blank">www.darmaxglobal.com</a>
-            
-
+            {`
+            <img width="200" height="60" src="${imageUrl}" />
             `}
           </pre>
           {/* <div dangerouslySetInnerHTML={{ __html: `<p>${greeting}</p><img src="${imageUrl}" alt="${name}" /><p>${name}</p>` }}></div> */}
